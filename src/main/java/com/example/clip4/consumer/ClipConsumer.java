6b34e662117c0271cc6b6a3b3c67c6d834f79521
이강훈 <khanylee@gmail.com>
2023-03-31 10:54:00 +0900

    get ConsumerRecordMetadata

--- src/main/java/com/example/clip4/consumer/ClipConsumer.java
+++ src/main/java/com/example/clip4/consumer/ClipConsumer.java
@@ -1,15 +1,18 @@
 package com.example.clip4.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClipConsumer {
 
-    @KafkaListener(id = "clip4-listener-id", topics = "clip4-listener", concurrency = "2", clientIdPrefix = "listener-id")
-    public void listen(String message) {
-        System.out.println("consume listener. message=" + message);
+    @KafkaListener(id = "clip4-listener-id", topics = "clip4-listener")
+    public void listen(String message,
+                       ConsumerRecordMetadata metadata) {
+        System.out.println("consume listener. timestamp="+ metadata.timestamp() + " type=" + metadata.timestampType());
+        System.out.println("consume listener. offset="+ metadata.offset() + " message=" + message);
     }
 
 }
