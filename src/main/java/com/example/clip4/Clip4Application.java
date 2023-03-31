1bd505ed5f2b2382cf196dc933d5b90ff8495257
이강훈 <khanylee@gmail.com>
2023-03-31 10:39:13 +0900

    kafkaMessageListenerContainer

--- src/main/java/com/example/clip4/Clip4Application.java
+++ src/main/java/com/example/clip4/Clip4Application.java
@@ -18,8 +18,23 @@ public class Clip4Application {
     public ApplicationRunner runner (ClipProducer clipProducer,
                                      KafkaMessageListenerContainer<String, String> kafkaMessageListenerContainer) {
         return args -> {
+            clipProducer.async("clip4", "Hello, Clip4 Container.");
+
             kafkaMessageListenerContainer.start();
+            Thread.sleep(1_000L);
+
+            System.out.println("-- pause --");
+            kafkaMessageListenerContainer.pause();
+            Thread.sleep(5_000L);
+
+            clipProducer.async("clip4", "Hello, Secondly Clip4 Container.");
+
+            System.out.println("-- resume --");
+            kafkaMessageListenerContainer.resume();
+            Thread.sleep(5_000L);
+
+            System.out.println("-- stop --");
+            kafkaMessageListenerContainer.stop();
         };
     }
-
 }
