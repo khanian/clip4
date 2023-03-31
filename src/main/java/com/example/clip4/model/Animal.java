55379a74dbf7b23f611777c2081a8340132da3db
이강훈 <khanylee@gmail.com>
2023-03-31 11:28:00 +0900

    KafkaJsonListenerContainerConfiguration

---
+++ src/main/java/com/example/clip4/model/Animal.java
@@ -0,0 +1,35 @@
+package com.example.clip4.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+
+public class Animal {
+
+    private final String name;
+    private final int age;
+
+
+    @JsonCreator
+    public Animal(@JsonProperty("name") String name,
+                  @JsonProperty("age") int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
